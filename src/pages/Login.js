e592cba93c6c8f8601405f0f92d3264b4b228e7e
Jéssica <jessicapmaximo@gmail.com>
2023-03-09 16:03:32 -0300

    organização inicial e renderização do formulário

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,39 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <div>
+        <form>
+          <label htmlFor="email">
+            E-mail
+            <input
+              data-testid="email-input"
+              id="email"
+              type="text"
+              name="email"
+            />
+          </label>
+
+          <label htmlFor="password">
+            Senha
+            <input
+              data-testid="password-input"
+              id="password"
+              type="text"
+              name="password"
+            />
+          </label>
+
+          <button
+            type="submit"
+
+          >
+            Entrar
+          </button>
+
+        </form>
+      </div>
+    );
   }
 }
 
--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,5 +1,15 @@
+import { combineReducers } from 'redux';
 // import user from './user';
 // import wallet from './wallet';
 
-// Configure os seus reducers.
-// ATENÇÃO: você obrigatoriamente tem que utilizar as chaves "user" e "wallet" no seu estado global
+const INITIAL_STATE = {};
+const exemplo = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+  default:
+    return state;
+  }
+};
+
+const rootReducer = combineReducers({ exemplo });
+
+export default rootReducer;
