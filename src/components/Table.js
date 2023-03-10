e94221ad8b279a3f947a6373ace4053fde98e4da
Jéssica <jessicapmaximo@gmail.com>
2023-03-10 17:10:14 -0300

    requisito 6 ok

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,9 +1,30 @@
 import React, { Component } from 'react';
 
 class Table extends Component {
+  constructor() {
+    super();
+    this.state = {
+
+    };
+  }
+
   render() {
     return (
-      <div>Table</div>
+      <table>
+        <caption>Alien football stars</caption>
+        <tr>
+          <th scope="col">Descrição</th>
+          <th scope="col">Tag</th>
+          <th scope="col">Método de pagamento</th>
+          <th scope="col">Valor</th>
+          <th scope="col">Moeda</th>
+          <th scope="col">Câmbio utilizado</th>
+          <th scope="col">Valor convertido</th>
+          <th scope="col">Moeda de conversão</th>
+          <th scope="col">Editar/Excluir</th>
+        </tr>
+
+      </table>
     );
   }
 }
